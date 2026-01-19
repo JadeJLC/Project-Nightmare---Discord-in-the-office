34d2792b67680b50da3fecd504608d20923f28d7
jcolange <jade.colanges@live.fr>
2026-01-19 12:58:43 +0100

    Structure de la page d'accueil

---
+++ variables/page-buttons.js
@@ -0,0 +1,16 @@
+// Boutons du header
+
+const LogInBtn = document.getElementById("log-in");
+const ProfileBtn = document.getElementById("display-profile");
+const ChatBtn = document.getElementById("display-online-users");
+const NotifBtn = document.getElementById("display-notifications");
+const FavBtn = document.getElementById("display-favorites");
+const ThemeSwitchBtn = document.getElementById("switch-theme");
+
+export { LogInBtn, ProfileBtn, ChatBtn, NotifBtn, FavBtn, ThemeSwitchBtn };
+
+// Boutons de la page d'accueil
+const CategBtn = document.getElementById("display-front-page-categ");
+const FeedBtn = document.getElementById("display-front-page-feed");
+
+export { CategBtn, FeedBtn };
