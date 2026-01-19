634b1f51c39ab316303476f981b32dd82f5bd6c8
jcolange <jade.colanges@live.fr>
2026-01-19 16:46:30 +0100

    CSS de base et header

--- variables/page-buttons.js
+++ variables/page-buttons.js
@@ -1,16 +1,15 @@
 // Boutons du header
+const logInBtn = document.getElementById("log-in");
+const profileBtn = document.getElementById("display-profile");
+const chatBtn = document.getElementById("display-online-users");
+const notifBtn = document.getElementById("display-notifications");
+const favBtn = document.getElementById("display-favorites");
+const themeSwitchBtn = document.getElementById("theme-selector");
 
-const LogInBtn = document.getElementById("log-in");
-const ProfileBtn = document.getElementById("display-profile");
-const ChatBtn = document.getElementById("display-online-users");
-const NotifBtn = document.getElementById("display-notifications");
-const FavBtn = document.getElementById("display-favorites");
-const ThemeSwitchBtn = document.getElementById("switch-theme");
-
-export { LogInBtn, ProfileBtn, ChatBtn, NotifBtn, FavBtn, ThemeSwitchBtn };
+export { logInBtn, profileBtn, chatBtn, notifBtn, favBtn, themeSwitchBtn };
 
 // Boutons de la page d'accueil
-const CategBtn = document.getElementById("display-front-page-categ");
-const FeedBtn = document.getElementById("display-front-page-feed");
+const categBtn = document.getElementById("display-front-page-categ");
+const feedBtn = document.getElementById("display-front-page-feed");
 
-export { CategBtn, FeedBtn };
+export { categBtn, feedBtn };
