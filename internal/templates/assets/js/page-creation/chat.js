c51a4b6b0e788787a8acbaec385eb29fa96f1222
npaccoud <nathpacc@gmail.com>
2026-02-04 21:42:43 +0100

    Header réactif pour la messagerie

--- internal/templates/assets/js/page-creation/chat.js
+++ internal/templates/assets/js/page-creation/chat.js
@@ -1,9 +1,14 @@
 // Fonctions pour la création de la page "messages privés" avec un utilisateur
 
+import { SessionData } from "../variables/session-data.js";
 import { clearPages } from "./clear-pages.js";
 
 export function displayMailbox() {
+  const usernameHeader = document.getElementById("header-username");
   const homeBtn = document.getElementById("go-home");
+  if (SessionData.isLogged) {
+    usernameHeader.innerHTML = `Messagerie de ${SessionData.username}`;
+  }
   homeBtn.style.display = "block";
   clearPages("dm");
 }
